196c4ad316a7c1d53f702f372e591a54bcecb6c4
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-05 22:25:08 +0100

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,67 +1,25 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import packageJson from './package.json' assert { type: 'json' };
+import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 
-const commonPlugins = [
-  peerDepsExternal(),
-  resolve(),
-  commonjs(),
-  babel({
-    babelHelpers: 'bundled',
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    extensions: ['.js', '.jsx'],
-    exclude: 'node_modules/**'
-  }),
-];
-
-const commonConfig = {
+export default {
   input: 'src/index.js',
-  external: ['preact', 'preact/hooks'],
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+  },
+  plugins: [
+    postcss({
+      extract: true,
+      minimize: true,
+      extensions: ['.css'],
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env'],
+      plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h', pragmaFrag: 'Fragment' }]],
+      extensions: ['.js', '.jsx'],
+    }),
+    terser(),
+  ],
 };
-
-export default [
-  // ESM build
-  {
-    ...commonConfig,
-    output: [
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [...commonPlugins],
-  },
-  // CommonJS build
-  {
-    ...commonConfig,
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-    ],
-    plugins: [...commonPlugins],
-  },
-  // UMD build
-  {
-    ...commonConfig,
-    output: [
-      {
-        file: packageJson.browser,
-        format: 'umd',
-        name: 'PreactHeadMaster',
-        globals: {
-          preact: 'preact',
-          'preact/hooks': 'preactHooks',
-        },
-        sourcemap: true,
-      },
-    ],
-    plugins: [...commonPlugins, terser()],
-  },
-];
